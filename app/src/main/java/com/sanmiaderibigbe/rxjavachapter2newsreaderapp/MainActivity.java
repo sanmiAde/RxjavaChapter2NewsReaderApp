f7a659dbcfbb61398f1420192d55928d20a2c06e
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-06-19 08:48:11 +0100

    initial commit.

---
+++ app/src/main/java/com/sanmiaderibigbe/rxjavachapter2newsreaderapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sanmiaderibigbe.rxjavachapter2newsreaderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
